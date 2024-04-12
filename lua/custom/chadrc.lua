ea39ca6b1f3c69eb50db9911fd0f6e583614fad0
ram pandey <iamram2510@gmail.com>
2024-04-12 20:15:01 +0530

    feat: added custom folder

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,7 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = { theme = 'tundra' }
+M.plugins = "custom.plugins"
+
+return M
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,81 @@
+local plugins = {
+  -- {
+  --   "mfussenegger/nvim-lint",
+  --   event = "VeryLazy",
+  --   config = function()
+  --     require "custom.configs.lint"
+  --   end
+  -- },
+  {
+    "mhartington/formatter.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.formatter"
+    end
+  },
+  {
+    "williamboman/mason.nvim",
+    opts={
+      ensure_installed={
+        "prettier",
+        "typescript-language-server",
+      }
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "tpope/vim-fugitive",
+    event = "VeryLazy",
+    config = function()
+      -- Configure vim-fugitive as merge tool
+      vim.cmd [[
+        set diffopt+=vertical
+        let g:fugitive_git_executable = 'git'
+        autocmd FileType fugitive setlocal bufhidden=delete
+      ]]
+    end,
+  },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+      "kevinhwang91/promise-async",
+      {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+          local builtin = require "statuscol.builtin"
+          require("statuscol").setup {
+            relculright = true,
+            segments = {
+              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+              { text = { "%s" }, click = "v:lua.ScSa" },
+              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+            },
+          }
+        end,
+      },
+    },
+    event = "BufReadPost",
+    config = function()
+      require("ufo").setup {
+        provider_selector = function()
+          return { "treesitter", "indent" }
+        end,
+      }
+    end,
+  },
+  {
+    "rainbowhxch/accelerated-jk.nvim",
+    dependencies = "nvim-treesitter",
+    event = "BufWinEnter",
+    config = function()
+      require "custom.configs.accelerated-jk"
+    end,
+  },
+}
+return plugins
